d2637029b753dcfe50bae2a23afe983bc6f0680f
FatherDmitry <fatherdmitry96@mail.ru>
2025-05-23 10:58:46 +0700

    Прописана часть кода для сохранения списка контактов в JSON

--- ContactManager/JsonStorage.cs
+++ ContactManager/JsonStorage.cs
@@ -8,5 +8,24 @@ using System.IO;
 
 public static class JsonStorage
 {
+    private static readonly string filePath = "contacts.json";
 
+    // Сохранение списка контактов в JSON
+    public static void Save(List<Contact> contacts) 
+    {
+        try
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            File.WriteAllText(filePath, JsonSerializer.Serialize(contacts, options));
+            MessageBox.Show("Контакты успешно сохранены", "Сохранение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Ошибка во время сохранения: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
 }
